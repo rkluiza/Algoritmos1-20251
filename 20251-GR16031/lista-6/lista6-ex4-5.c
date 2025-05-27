fc2a517be514f8c1e222c17225515ff94798345a
luiza klein <kleinluiza9@gmail.com>
2025-05-27 20:14:42 -0300

    lista-6

--- 20251-GR16031/lista-6/lista6-ex4-5.c
+++ 20251-GR16031/lista-6/lista6-ex4-5.c
@@ -1,3 +1,15 @@
+/*
+
+Escreva um algoritmo que preenche uma matriz 4x6 com valores inteiros aleatórios entre -10 e
+10. Calcule as somas:
+a. dos elementos da segunda linha
+b. dos elementos da quinta coluna
+c. da multiplicação dos elementos da primeira linha pelos elementos da quarta linha
+d. dos elementos só das colunas com índices pares
+e. dos elementos só das linhas com índices ímpares
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -5,33 +17,48 @@
 int main(){
 
     srand(time(0));
-
+    
     int matriz[4][6];
-    int seg_linha =0, qui_coluna =0;
+    int soma_linha2 = 0, soma_coluna5 = 0, soma_mult_linha1_4 = 0;
+    int soma_colunas_pares = 0, soma_linhas_impares = 0;
 
-    printf("Escreva os números de uma matriz 4x6");
-    
-    for (int i=0; i<4; i++){
-        for (int j=0; j<6; j++){
-            matriz[i][j] = -10 + rand() % 21; // valores de -10 a 10;
-            printf("%d, ", matriz[i][j]);
-        
-        if(i==1){
-            seg_linha += matriz[i][j];
+    // Preenchendo a matriz com valores aleatórios entre -10 e 10
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 6; j++) {
+            matriz[i][j] = -10 + rand() % 21; // Valores entre -10 e 10
+            printf("%d ", matriz[i][j]);
         }
-    
-        if(j==4){
-            qui_coluna += matriz[i][j];
-        }
-
-
-        
-        }
-
         printf("\n");
     }
 
-    
-//incompleto
+    // Calculando as somas
+    for(int j = 0; j < 6; j++) {
+        soma_linha2 += matriz[1][j]; // Soma da segunda linha
+        if (j == 4) {
+            soma_coluna5 += matriz[0][j]; // Soma da quinta coluna
+        }
+        if (j % 2 == 0) {
+            for(int i = 0; i < 4; i++) {
+                soma_colunas_pares += matriz[i][j]; // Soma das colunas pares
+            }
+        }
+    }
+
+    for(int i = 0; i < 4; i++) {
+        if (i % 2 != 0) { // Linhas ímpares
+            for(int j = 0; j < 6; j++) {
+                soma_linhas_impares += matriz[i][j];
+            }
+        }
+        soma_mult_linha1_4 += matriz[i][0] * matriz[i][3]; // Multiplicação da primeira linha pela quarta linha
+    }
+
+    // Exibindo os resultados
+    printf("Soma dos elementos da segunda linha: %d\n", soma_linha2);
+    printf("Soma dos elementos da quinta coluna: %d\n", soma_coluna5);
+    printf("Soma da multiplicação dos elementos da primeira linha pelos elementos da quarta linha: %d\n", soma_mult_linha1_4);
+    printf("Soma dos elementos só das colunas com índices pares: %d\n", soma_colunas_pares);
+    printf("Soma dos elementos só das linhas com índices ímpares: %d\n", soma_linhas_impares);
+    return 0;
 
 }
